d4957754a996be6ea986ebd4a4f82baa7b95d8e9
MattAgn <matthieua@bam.tech>
2020-03-16 22:55:23 +0100

    :sparkles: (Options) add fields

--- lib/screens/options.dart
+++ lib/screens/options.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Options extends StatelessWidget {
+class Options extends StatefulWidget {
+  @override
+  _OptionsState createState() => _OptionsState();
+}
+
+class _OptionsState extends State<Options> {
+  List<bool> isSelected = [false, true];
+  int nbWalls = 0;
+  int nbPortals = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,8 +17,69 @@ class Options extends StatelessWidget {
         title: Text("Options"),
       ),
       body: Container(
-        child: Column(
-          children: <Widget>[],
+        padding: EdgeInsets.all(30),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text("Surround board with"),
+                  SizedBox(height: 5),
+                  ToggleButtons(
+                    children: <Widget>[Text("Walls"), Text("Portals")],
+                    isSelected: isSelected,
+                    onPressed: (int index) {
+                      setState(() {
+                        for (int buttonIndex = 0;
+                            buttonIndex < isSelected.length;
+                            buttonIndex++) {
+                          if (buttonIndex == index) {
+                            isSelected[buttonIndex] = true;
+                          } else {
+                            isSelected[buttonIndex] = false;
+                          }
+                        }
+                      });
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 40),
+              Column(
+                children: <Widget>[
+                  Text("Number of walls"),
+                  SizedBox(height: 5),
+                  Slider(
+                    value: nbWalls.toDouble(),
+                    max: 20,
+                    min: 0,
+                    onChanged: (value) => setState(() {
+                      nbWalls = value.toInt();
+                    }),
+                  ),
+                  Text(nbWalls.toString())
+                ],
+              ),
+              SizedBox(height: 40),
+              Column(
+                children: <Widget>[
+                  Text("Number of portals"),
+                  SizedBox(height: 5),
+                  Slider(
+                    value: nbPortals.toDouble(),
+                    max: 20,
+                    min: 0,
+                    onChanged: (value) => setState(() {
+                      nbPortals = value.toInt();
+                    }),
+                  ),
+                  Text(nbPortals.toString())
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
